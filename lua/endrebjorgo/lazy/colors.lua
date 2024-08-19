d6cd2416af7b2efa6e84364b1d791c3e2245d5de
endrebjorgo <endrebjorgo@gmail.com>
2024-08-19 11:53:24 +0200

    Add obsidian

--- lua/endrebjorgo/lazy/colors.lua
+++ lua/endrebjorgo/lazy/colors.lua
@@ -1,12 +1,10 @@
 return {
-    {
-        'aktersnurra/no-clown-fiesta.nvim',
-        name = "no-clown-fiesta",
-        config = function()
-            vim.cmd('colorscheme no-clown-fiesta')
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-            vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#111111" })
-        end
-    }
+    'aktersnurra/no-clown-fiesta.nvim',
+    name = "no-clown-fiesta",
+    config = function()
+        vim.cmd('colorscheme no-clown-fiesta')
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#111111" })
+    end
 }
