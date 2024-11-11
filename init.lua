4efa349683ed900cbece97eb16ad0cfd5c715780
endrebjorgo <endrebjorgo@gmail.com>
2024-11-11 15:59:55 +0100

    Add automatic pull at launch

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
+local cmd = 'silent !git -C ' .. vim.fn.stdpath("config") .. ' pull --ff-only'
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        vim.cmd(cmd)
+    end,
+})
 require("endrebjorgo")
