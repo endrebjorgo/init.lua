3a7a24cc4057e6630d66778bc082bfa86d634087
endrebjorgo <endrebjorgo@gmail.com>
2024-11-21 22:42:41 +0100

    Edit colors

--- lua/endrebjorgo/lazy/colors.lua
+++ lua/endrebjorgo/lazy/colors.lua
@@ -2,9 +2,9 @@ return {
     "aktersnurra/no-clown-fiesta.nvim",
     name = "no-clown-fiesta",
     config = function()
+        require("no-clown-fiesta").setup({
+            transparent = true,
+        })
         vim.cmd('colorscheme no-clown-fiesta')
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#111111" })
     end
 }
